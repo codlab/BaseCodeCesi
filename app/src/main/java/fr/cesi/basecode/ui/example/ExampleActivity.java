b9197ebeac37a44d053d6a76ac728fc6245188e0
Kevin Le Perf <codlabtech@gmail.com>
2017-06-21 14:16:04 +0200

    initial version

---
+++ app/src/main/java/fr/cesi/basecode/ui/example/ExampleActivity.java
@@ -0,0 +1,18 @@
+package fr.cesi.basecode.ui.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import fr.cesi.basecode.R;
+
+public class ExampleActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_example);
+    }
+}
