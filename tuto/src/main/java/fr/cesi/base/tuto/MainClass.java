6cf841e9417af64c33e7ab1ac607ad17137734cb
Kevin Le Perf <codlabtech@gmail.com>
2017-06-22 20:18:32 +0200

    add excerpt for java tutorial

---
+++ tuto/src/main/java/fr/cesi/base/tuto/MainClass.java
@@ -0,0 +1,14 @@
+package fr.cesi.base.tuto;
+
+/**
+ * Created by kevinleperf on 22/06/2017.
+ */
+
+public class MainClass {
+
+    public static void main(String[] argv) {
+        System.out.println("tuto started ...");
+
+        System.out.println("tuto ended ...");
+    }
+}
