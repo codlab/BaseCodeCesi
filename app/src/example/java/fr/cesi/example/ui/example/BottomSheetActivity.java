6ced4a165dce63657921296515351a43c48d33ca
Kevin Le Perf <codlabtech@gmail.com>
2017-06-22 19:48:16 +0200

    use flavor to extract example from the base package

---
+++ app/src/example/java/fr/cesi/example/ui/example/BottomSheetActivity.java
@@ -0,0 +1,41 @@
+package fr.cesi.example.ui.example;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.flipboard.bottomsheet.BottomSheetLayout;
+
+import fr.cesi.example.R;
+
+public class BottomSheetActivity extends AppCompatActivity {
+
+    private Button _show_bottom_sheet_button;
+    private BottomSheetLayout _bottom_sheet_layout;
+    private View _bottom_sheet_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_bottom_sheet);
+
+        _show_bottom_sheet_button = (Button) findViewById(R.id.show_bottom_sheet);
+        _bottom_sheet_layout = (BottomSheetLayout) findViewById(R.id.bottomsheet);
+
+        _bottom_sheet_view = LayoutInflater.from(this)
+                .inflate(R.layout.view_bottom_sheet, _bottom_sheet_layout,
+                        /*take the parent but do not add the view into the layout*/
+                        false);
+
+        _show_bottom_sheet_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+            }
+        });
+    }
+
+}
