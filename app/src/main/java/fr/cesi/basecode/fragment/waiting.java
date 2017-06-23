18c6ac5a103550401ef6c9c5f44ff53da1581677
polostosorus <paul.faro@gmail.com>
2017-06-23 16:31:36 +0200

    Splash activity

--- app/src/main/java/fr/cesi/basecode/fragment/waiting.java
+++ app/src/main/java/fr/cesi/basecode/fragment/waiting.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import fr.cesi.basecode.example.R;
+import fr.cesi.basecode.fragment.R;
 
 /**
  * A simple {@link Fragment} subclass.
