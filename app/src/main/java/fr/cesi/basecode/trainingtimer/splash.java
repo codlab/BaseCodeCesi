f42ed12e30e50c718af8932f1d1da6377aab6069
@bouloan <boulord.anthony@hotmail.com>
2017-06-23 15:52:02 +0200

    creation activités et fragments

---
+++ app/src/main/java/fr/cesi/basecode/trainingtimer/splash.java
@@ -0,0 +1,17 @@
+package fr.cesi.basecode.trainingtimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.cesi.basecode.R;
+
+
+public class splash extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
