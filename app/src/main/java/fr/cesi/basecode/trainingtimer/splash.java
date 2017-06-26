c9ed401546c76e08a5350069da77b185ddeeca8b
polostosorus <paul.faro@gmail.com>
2017-06-26 14:21:30 +0200

    modifs inutiles

--- app/src/main/java/fr/cesi/basecode/trainingtimer/splash.java
+++ app/src/main/java/fr/cesi/basecode/trainingtimer/splash.java
@@ -1,17 +1,19 @@
 package fr.cesi.basecode.trainingtimer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import fr.cesi.basecode.R;
 
 
-public class splash extends AppCompatActivity {
 
+public class splash extends AppCompatActivity {
+    private Handler _handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
     }
+
 }
