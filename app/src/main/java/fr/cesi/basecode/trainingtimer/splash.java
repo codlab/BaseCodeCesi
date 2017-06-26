1117dc687e323883516c4528b3e0b0b86ccc2099
@bouloan <boulord.anthony@hotmail.com>
2017-06-26 14:19:21 +0200

    réparation android manifest

--- app/src/main/java/fr/cesi/basecode/trainingtimer/splash.java
+++ app/src/main/java/fr/cesi/basecode/trainingtimer/splash.java
@@ -1,5 +1,7 @@
 package fr.cesi.basecode.trainingtimer;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,10 +10,27 @@ import fr.cesi.basecode.R;
 
 public class splash extends AppCompatActivity {
 
+    private Handler _handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        _handler = new Handler();
+
+        _handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startNextActivity();
+            }
+        }, 3000);
+    }
+
+    private void startNextActivity() {
+        Intent intent = new Intent(this, timer.class);
+        startActivity(intent);
+        finish();
     }
 }
+
