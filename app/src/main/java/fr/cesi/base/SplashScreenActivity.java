f4c29c56667612772c662328c8a3c94e9fd53b4f
v4n35sa <van.navarro.1@gmail.com>
2017-06-26 16:32:56 +0200

    correctif splash screen

--- app/src/main/java/fr/cesi/base/SplashScreenActivity.java
+++ app/src/main/java/fr/cesi/base/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package fr.cesi.base;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +9,12 @@ import fr.cesi.basecode.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private Handler _handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        _hander = new Handler();
+        _handler = new Handler();
 
         _handler.postDelayed(new Runnable() {
             @Override
@@ -23,7 +25,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
 
     private void startNextActivity() {
-        Intent intent = new Intent(this.TimerActivity.class);
+        //Intent intent = new Intent(TimerActivity.class);
 
         finish();
     }
