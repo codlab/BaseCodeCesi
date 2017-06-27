9ed2b35af20b390a03d3d0842b713ca36a543715
Ur Father <fred.geffray@gmail.com>
2017-06-27 15:07:10 +0200

    text question

--- app/src/main/java/fr/cesi/ui/question_fragment.java
+++ app/src/main/java/fr/cesi/ui/question_fragment.java
@@ -17,12 +17,19 @@ import fr.cesi.basecode.R;
 public class question_fragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
+    private static final String ARG_ASK = "ask";
+    private static final String ARG_IMG = "img";
+    private static final String ARG_BTN1 = "btn1";
+    private static final String ARG_BTN2 = "btn2";
+    private static final String ARG_BTN3 = "btn3";
+    private static final String ARG_BTN4 = "btn4";
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String ask;
+    private String img;
+    private String btn1;
+    private String btn2;
+    private String btn3;
+    private String btn4;
 
 
     public question_fragment() {
@@ -38,11 +45,15 @@ public class question_fragment extends Fragment {
      * @return A new instance of fragment question_fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static question_fragment newInstance(String param1, String param2) {
+    public static question_fragment newInstance(String text, int image, String bouton1, String bouton2, String bouton3, String bouton4 ) {
         question_fragment fragment = new question_fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_ASK, ask);
+        args.putString(ARG_IMG, img);
+        args.putString(ARG_BTN1, btn1);
+        args.putString(ARG_BTN2, btn2);
+        args.putString(ARG_BTN3, btn3);
+        args.putString(ARG_BTN4, btn4);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,8 +62,12 @@ public class question_fragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            ask = getArguments().getString(ARG_ASK);
+            img = getArguments().getString(ARG_IMG);
+            btn1 = getArguments().getString(ARG_BTN1);
+            btn2 = getArguments().getString(ARG_BTN2);
+            btn3 = getArguments().getString(ARG_BTN3);
+            btn4 = getArguments().getString(ARG_BTN4);
         }
     }
 
@@ -64,3 +79,4 @@ public class question_fragment extends Fragment {
     }
 
 }
+
