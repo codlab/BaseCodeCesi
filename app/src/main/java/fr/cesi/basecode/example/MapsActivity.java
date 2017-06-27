3bdd9413166548ec74f606e27dba2c6360fd2887
Dominic Tayler <dominic.tayler@gmail.com>
2017-06-27 15:35:59 +0200

    add bottom sheet

--- app/src/main/java/fr/cesi/basecode/example/MapsActivity.java
+++ app/src/main/java/fr/cesi/basecode/example/MapsActivity.java
@@ -3,15 +3,14 @@
  */
 package fr.cesi.basecode.example;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import fr.cesi.basecode.R;
 
@@ -35,3 +34,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 }
+
+    private BottomSheetBehavior mBottomSheetBehavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View bottomSheet = findViewById( R.id.bottom_sheet );
+        Button button1 = (Button) findViewById( R.id.button_1 );
+
+        button1.setOnClickListener(this);
+
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch( v.getId() ) {
+            case R.id.button_1: {
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                break;
+            }
+        }
+    }
